ab0686d265caf74aabbcac69b027f5e4ee8ef2ba
Casey Reas <reas@reas.com>
2010-06-21 21:54:14 +0000

    New generate page for Environment distro file

--- generate/environment_local.php
+++ generate/environment_local.php
@@ -1,31 +1,10 @@
 <?
-/**
 require('../config.php');
 require('lib/Translation.class.php');
 $benchmark_start = microtime_float();
 
-// arguments
 $lang = isSet($_POST['lang']) ? $_POST['lang'] : 'en';
-
-// get translation file
-$translation = new Translation($lang);
-
-// make overview page
-$source = CONTENTDIR."/api_$lang/environment/";
-$path = REFERENCEDIR . ($lang == 'en' ? '' : "/$lang") . "/environment/";
-make_necessary_directories($path."images/file");
-$page = new Page("Environment (IDE)", "Environment", "Environment");
-$page->content(file_get_contents($source."index.html"));
-$page->language($lang);
-writeFile('reference/'.($lang=='en'?'':"$lang/").'environment/index.html', $page->out());
-copydirr($source.'/images', $path.'/images');
-*/
-require('../config.php');
-require('lib/Translation.class.php');
-$benchmark_start = microtime_float();
-
-//$lang = isSet($_POST['lang']) ? $_POST['lang'] : 'en';
-$lang = 'en';
+//$lang = 'en';
 echo "Language selected";
 
 $source = CONTENTDIR."/api_$lang/environment/";
@@ -34,22 +13,9 @@ $path = DISTDIR."/environment/";
 // get translation file
 $translation = new Translation($lang);
 
-// make directories if they are not already there
-if (!is_dir($path)) { 
-	//mkdir($path, '0775'); 
-	echo "Environment folder created";
-
-}
-
-if (!is_dir($path.'images/')) { 
-	//mkdir($path.'images/', '0775'); 
-	echo "Image folder created";
-
-}
-
 // make page
 
-//make_necessary_directories($path."images/file");
+make_necessary_directories($path."images/file");
 $page = new LocalPage('Environment (IDE)', 'Environment', 'Environment', '../');
 $page->content(file_get_contents($source.'index.html'));
 $page->language($lang);
