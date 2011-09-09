a082b42a91b5381d8b4e4cd57712d9a5937887ab
pesckal <pkalauskas@gmail.com>
2011-09-09 04:13:39 +0000

    No more XML for python script

--- contrib_generate/build_listing.py
+++ contrib_generate/build_listing.py
@@ -37,33 +37,27 @@ category. The config file is formatted using the following style:
     AwesomeLibs \ http://example.net/libs/awesomelibs.txt
 
 After parsing the config file, information on each library is retrieved from
-the text files, and the contribution is assumed to be hosted at the same address
-as the txt, but with .zip.  For example, visionlib in the example above
+the text files, and the contribution is assumed to be hosted at the same
+address as the txt, but with .zip.  For example, visionlib in the example above
 would be found at http://example.com/libs/visionlib.zip
 
 This script takes two arguments
   Arg 1: The name of a config file to read from
-  Arg 2: The name of an xml file to write to. This file will be overwritten
-         if it already exists
+  Arg 2: The name of an file to write to. This file will be overwritten if it
+         already exists
 """
 
 from sys import argv
 from urllib2 import *
-from xml.etree.ElementTree import *
 import re
 import shutil
+import httplib
 
-LIBRARY = 'library'
-LIBRARY_COMPILATION = 'librarycompilation'
-TOOL = 'tool'
-MODE = 'mode'
 ENCODING = 'UTF-8'
-#ENCODING = 'us-ascii'
 
-def get_exports(f):
+def read_exports(f):
   """
-  Reads a library's export.txt file and returns a dictionary of
-  attributes and values.
+  Reads a library's export.txt file and returns a dictionary.
   """
   export_table = {}
   for line in f.readlines():
@@ -80,74 +74,16 @@ def get_exports(f):
     export_table[attr] = val
   return export_table
 
-
-def autoindent(elem, level=0, is_last=True):
+def write_exports(f, exports):
   """
-  Appends whitespace before and after each XML element to make the printed
-  XML more readable
+  Writes the key-value pairs from the given dictionary to a file in the form
+  key1=value1
+  key2=value2
   """
-
-  if len(elem) and not elem.text:
-    elem.text = '\n'  + (level + 1) * ' '
-
-  if not elem.tail or not elem.tail.strip():
-    if is_last:
-      elem.tail = '\n' + (level - 1) * ' '
-    else:
-      elem.tail = '\n' + level * ' '
-
-  for next_elem in elem:
-    is_last = next_elem == elem[-1]
-    autoindent(next_elem, level + 1, is_last)
-
-
-def create_common_element(exports, tag, download_url):
-  """
-  Creates an XML element for a contribution using a dictionary containing
-  attributes from its export file and a url that's a direct-link to
-  download it
-
-  Throws a KeyError if one of the required attributes isn't in exports.
-  Required keys are 'name', 'authorList', 'url', 'sentence', and 'version'
-  """
-  libelement = Element(tag)
-  libelement.set('name', exports['name'])
-  libelement.set('url', exports['url'])
-
-  description = Element('description')
-  description.set('authorList', exports['authorList'])
-  description.set('sentence', exports['sentence'])
-  if exports.has_key('paragraph'):
-    description.set('paragraph', exports['paragraph'])
-  libelement.append(description)
-
-  version = Element('version')
-  version_id = exports['version']
-  version.set('id', version_id)
-  if exports.has_key('prettyVersion'):
-    version.set('pretty', exports['prettyVersion'])
-  else:
-    version.set('pretty', version_id)
-
-  libelement.append(version)
-
-  has_explicit_links = False
-  location = Element('download')
-  for attr, value in exports.iteritems():
-    if attr.startswith('download'):
-      has_explicit_links = True
-      dot = attr.find('.')
-      if dot == -1:
-        location.set('other', value)
-      else:
-        location.set(attr[dot + 1:], value)
-  
-  if not has_explicit_links:
-    location.set('other', download_url)
-
-  libelement.append(location)
-
-  return libelement
+  for key in exports:
+    key = key.encode(ENCODING)
+    value = exports[key].encode(ENCODING)
+    f.write('%s=%s\n' % (key, value))
 
 def get_lib_locations(f):
   """
@@ -195,57 +131,53 @@ def get_lib_locations(f):
 
   return urls_by_category
 
+def has_required_keys(exports):
+  keys = exports.keys()
+  return keys.count('name') and keys.count('authorList') and keys.count('url') and keys.count('category') and keys.count('sentence') and keys.count('version')
+
 if __name__ == "__main__":
   if len(argv) != 3:
-    print "Usage is [Input Configuration File] [Output XML file]"
+    print "Usage is [Input Configuration File] [Output file]"
     exit()
 
-  script, conf, xmlout = argv
+  script, conf, fileout = argv
   f = open(conf)
   urls_by_category = get_lib_locations(f);
   f.close()
 
-  software = Element('software')
+  tmpfile = fileout + '.tmp'
+  f = open(tmpfile, 'w')
 
   for cat in urls_by_category:
-    category = Element('category')
-    category.set('name', cat)
-    software.append(category)
     contribs = urls_by_category[cat]
 
     for contrib in contribs:
       software_type, name, prop_url = contrib
       download_url = prop_url[:prop_url.rfind('.')] + '.zip'
       try:
-        element = None
-        exports = get_exports(urlopen(prop_url))
+        exports = read_exports(urlopen(prop_url))
 
-        # Overwrite the name with what was in the .conf file
+        # overwrite the name and category with what was in the .conf file
         exports['name'] = name
+        exports['category'] = cat
 
-        if software_type == LIBRARY:
-          element = create_common_element(exports, 'library', download_url)
-        elif software_type == LIBRARY_COMPILATION:
-          element = create_common_element(exports, 'librarycompilation', download_url)
-        elif software_type == TOOL:
-          element = create_common_element(exports, 'tool', download_url)
-        elif software_type == MODE:
-          element = create_common_element(exports, 'mode', download_url)
+        if not has_required_keys(exports):
+          print 'Error reading', prop_url
+          print '  No value for "' + inst.args[0] + '". Maybe it\'s a 404 page'
+          continue
+        # if no download is explicitly provided, use the default download url
+        m = re.compile('download.*')
+        if len([x for x in exports if m.match(x)]) == 0:
+          exports['download'] = download_url
 
-        if element != None:
-          category.append(element)
+        f.write('%s\n' % software_type)
+        write_exports(f, exports)
+        f.write('\n')
+
       except IOError as inst:
         print 'Error reading', prop_url
         print inst
-      except KeyError as inst:
-        print 'Error reading', prop_url
-        print '  No value for "' + inst.args[0] + '". Maybe it\'s a 404 page'
-
-  software_tree = ElementTree()
-  autoindent(software)
-  software_tree._setroot(software)
   
-  tmpfile = xmlout + '.tmp'
-  software_tree.write(tmpfile, ENCODING, True)
-  shutil.move(tmpfile, xmlout)
+  f.close()
+  shutil.move(tmpfile, fileout)
 
