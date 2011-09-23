de899c062df02735b1bc0f082a6ad925775eb2ed
Casey Reas <reas@reas.com>
2011-09-23 03:28:27 +0000

    Improved Examples 2 Generate script

--- generate/examples.php
+++ generate/examples.php
@@ -4,23 +4,18 @@ require('../config.php');
 require('lib/Example.class.php');
 $benchmark_start = microtime_float();
 
-$local = true;
+//$local = true;
 
 // Redefine location on local machine for examples in processing trunk
-if ($local) {
+//if ($local) {
   //define('BASEDIR',    '/Users/REAS/Documents/reas@processing.org/trunk/');
-  define('EXAMPLESOURCEDIR', '/Users/REAS/Documents/reas@processing.org/trunk/processing/java/examples/');
-} else {
-  define('EXAMPLESOURCEDIR', '/Users/REAS/Documents/reas@processing.org/trunk/processing/java/examples/');
-}
+  //define('EXAMPLESOURCEDIR', '/Users/REAS/Documents/reas@processing.org/trunk/processing/java/examples/');
+//} else {
+  define('EXAMPLESOURCEDIR', '../../processing/java/examples/');
+//}
 //define('CONTENTDIR', BASEDIR.'processing/java/');
 //define('EXAMPLESDIR',	BASEDIR.'learning/');
 
-
-// Update the files on the server via SVN
-
-// look for the .subversion folder somewhere else
-// otherwise will go looking for /home/root/.subversion or some other user
 $where = CONTENTDIR . 'examples';
 $there = CONTENTDIR;
 putenv('HOME=' . CONTENTDIR);
@@ -28,11 +23,11 @@ putenv('HOME=' . CONTENTDIR);
 // do the initial checkout
 //`cd /var/www/processing && /usr/local/bin/svn co svn://processing.org/trunk/web/content/`;
 
-if (!$local) {
-  `cd $there && /usr/local/bin/svn update examples_basics.xml`;
-  `cd $there && /usr/local/bin/svn update examples_topics.xml`;
-  `cd $where && /usr/local/bin/svn update`;
-}
+//if (!$local) {
+`cd $there && /usr/local/bin/svn update examples_basics.xml`;
+`cd $there && /usr/local/bin/svn update examples_topics.xml`;
+`cd $where && /usr/local/bin/svn update`;
+//}
 
 
 # --------------------------------- Basics
@@ -67,7 +62,6 @@ $page->subtemplate('template.examples-basics.html');
 $html = "<div class=\"ref-col\">\n";
 foreach ($categories as $cat => $array) {
     
-    #$html .= "<h3><img src=\"images/".strtolower(removesymbols($cat)).".gif\" alt=\"$cat\" /></h3>\n<p>";
     $html .= "<p><br /><b>$cat</b><br /><br />";
     foreach ($array as $file => $name) {
         $thisfile = strtolower($file);
@@ -84,16 +78,6 @@ $html .= "</div>";
 
 $page->content($html);
 writeFile('learning/'.strtolower($subdir).'/index.html', $page->out());
-
-// Copy over a shared core.jar
-//if (!copy(CONTENTDIR . 'examples/core.jar',
-//	  EXAMPLESDIR.strtolower($subdir).'/media/core.jar')) {
-//  echo 'Could not copy core.jar to ' . $subdir . '.';
-//}
-//if (!copy(CONTENTDIR . 'examples/loading.gif',
-//	  EXAMPLESDIR.strtolower($subdir).'/media/loading.gif')) {
-//  echo 'Could not copy loading.gif to ' . $subdir . '.';
-//}
 
 
 # --------------------------------- Topics
@@ -126,8 +110,6 @@ $page->subtemplate('template.examples-topics.html');
 
 $html = "<div class=\"ref-col\">\n";
 foreach ($categories as $cat => $array) {
-    
-    #$html .= "<h3><img src=\"images/".strtolower(removesymbols($cat)).".gif\" alt=\"$cat\" /></h3>\n<p>";
     $html .= "<p><br /><b>$cat</b><br /><br />";
     foreach ($array as $file => $name) {
         $thisfile = strtolower($file);
@@ -144,74 +126,6 @@ $html .= "</div>";
 
 $page->content($html);
 writeFile('learning/'.strtolower($subdir).'/index.html', $page->out());
-
-/**
-if (!copy(CONTENTDIR . 'examples/core.jar',
-	  EXAMPLESDIR.strtolower($subdir).'/media/core.jar')) {
-  echo 'Could not copy core.jar to ' . $subdir . '.';
-}
-if (!copy(CONTENTDIR . 'examples/loading.gif',
-	  EXAMPLESDIR.strtolower($subdir).'/media/loading.gif')) {
-  echo 'Could not copy loading.gif to ' . $subdir . '.';
-}
-*/
-
-/**
-# --------------------------------- 3D
-
-
-$categories = get_examples_list('examples_3D.xml');
-$break_after = array('Transform', 'Lights');
-$subdir = '3D';
-$dir = CONTENTDIR.'examples/'.$subdir.'/';
-
-$count = 0;
-foreach ($categories as $cat => $array) {
-    if ($dp = opendir($dir.$cat)) {
-        while ($fp = readdir($dp)) {
-            if (substr($fp, 0, 1) != '.') {
-                $ex = new Example($fp, $subdir."/".$cat, $subdir);
-                //$ex = new Example($fp, $cat);
-                $ex->output_file($categories);
-                $count++;
-            }
-        }
-    }
-}
-
-$page = new Page('3D', '3D');
-$page->subtemplate('template.examples-3d.html');
-
-$html = "<div class=\"ref-col\">\n";
-foreach ($categories as $cat => $array) {
-    
-    #$html .= "<h3><img src=\"images/".strtolower(removesymbols($cat)).".gif\" alt=\"$cat\" /></h3>\n<p>";
-    $html .= "<p><br /><b>$cat</b><br /><br />";
-    foreach ($array as $file => $name) {
-        $thisfile = strtolower($file);
-        $html .= "\t<a href=\"$thisfile\">$name</a><br />\n";
-    }
-    #echo '</p>';
-    $html .= '</p>';
-    
-    if (in_array($cat, $break_after)) {
-        $html .= "</div><div class=\"ref-col\">";
-    }
-}
-$html .= "</div>";
-
-$page->content($html);
-writeFile('learning/'.strtolower($subdir).'/index.html', $page->out());
-if (!copy(CONTENTDIR . 'examples/core.jar',
-	  EXAMPLESDIR.strtolower($subdir).'/media/core.jar')) {
-  echo 'Could not copy core.jar to ' . $subdir . '.';
-}
-if (!copy(CONTENTDIR . 'examples/loading.gif',
-	  EXAMPLESDIR.strtolower($subdir).'/media/loading.gif')) {
-  echo 'Could not copy loading.gif to ' . $subdir . '.';
-}
-
-*/
 
 
 $benchmark_end = microtime_float();
