c5db35f3f8d66766462eb87dfbecc13355a27e73
amongiants <jmgacnik@gmail.com>
2013-04-13 19:40:42 -0700

    .gitignore fix

---
+++ javascript/stickyNav.js
@@ -0,0 +1,9 @@
+// $(function(){
+// 	$(window).scroll(function(){
+// 		if($(this).scrollTop() > 130){
+// 			$('.navBar').addClass('stuck')
+// 		} else {
+// 			$('.navBar').removeClass('stuck')
+// 		}
+// 	})
+// })
