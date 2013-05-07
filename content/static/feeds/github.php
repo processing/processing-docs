3d037a54452103dc3fee33bc78bc4f1e2c6ec75b
amongiants <jmgacnik@gmail.com>
2013-05-07 13:04:20 -0700

    Testing github cacher

--- content/static/feeds/github.php
+++ content/static/feeds/github.php
@@ -3,40 +3,40 @@
 //cache the json file and only call again if interval exceeded
 error_reporting(0);
 
-function get_json($url){
-  $base = "https://api.github.com/";
-  $curl = curl_init();
-  curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-  curl_setopt($curl, CURLOPT_URL, $base . $url);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+// function get_json($url){
+//   $base = "https://api.github.com/";
+//   $curl = curl_init();
+//   curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+//   curl_setopt($curl, CURLOPT_URL, $base . $url);
+//   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+//   curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-  $content = curl_exec($curl);
-  curl_close($curl);
-  return $content;
-}
+//   $content = curl_exec($curl);
+//   curl_close($curl);
+//   return $content;
+// }
 
-$feed = "repos/processing/processing/commits";
-$cache_file = dirname(__FILE__).'/cache/'.'github-cache';
-$modified = filemtime( $cache_file );
-$now = time();
-$interval = 600; // ten minutes
+// $feed = "repos/processing/processing/commits";
+// $cache_file = dirname(__FILE__).'/cache/'.'github-cache';
+// $modified = filemtime( $cache_file );
+// $now = time();
+// $interval = 600; // ten minutes
  
-if ( !$modified || ( ( $now - $modified ) > $interval ) ) {
-  $json = get_json($feed);
+// if ( !$modified || ( ( $now - $modified ) > $interval ) ) {
+//   $json = get_json($feed);
   
-  if ( $json ) {
-    $cache_static = fopen( $cache_file, 'w' );
-    fwrite( $cache_static, $json );
-    fclose( $cache_static );
-  }
-}
+//   if ( $json ) {
+//     $cache_static = fopen( $cache_file, 'w' );
+//     fwrite( $cache_static, $json );
+//     fclose( $cache_static );
+//   }
+// }
  
-header( 'Cache-Control: no-cache, must-revalidate' );
-header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' );
-header( 'Content-type: application/json' );
+// header( 'Cache-Control: no-cache, must-revalidate' );
+// header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' );
+// header( 'Content-type: application/json' );
  
-$json = file_get_contents( $cache_file );
-echo $json;
+// $json = file_get_contents( $cache_file );
+// echo $json;
 
 ?>
