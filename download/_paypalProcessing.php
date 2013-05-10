d40eba3f1e0bd7ac82949fcb2a4a84f867b87fca
amongiants <jmgacnik@gmail.com>
2013-05-10 14:45:36 -0700

    Switching paypal out of sandbox mode

--- download/_paypalProcessing.php
+++ download/_paypalProcessing.php
@@ -4,10 +4,10 @@ require_once('./paypal/paypal.php');
 require_once('./paypal/httprequest.php');
 
 //Use this form for production server 
-//$r = new PayPal(true);
+$r = new PayPal(true);
 
 //Use this form for sandbox tests
-$r = new PayPal();
+// $r = new PayPal();
 
 $amount = (float) $_POST['amount'];
 $ret = ($r->doExpressCheckout($amount, 'Donation to the Processing Foundation'));
