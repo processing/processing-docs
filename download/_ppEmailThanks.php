cf71043e6e76ebede39976f0459e5da5edfbb33d
amongiants <jon@folderstudio.com>
2014-06-06 16:33:00 -0700

    cleaning up timestamp in the purchase log file

--- download/_ppEmailThanks.php
+++ download/_ppEmailThanks.php
@@ -57,8 +57,7 @@ if ($final['ACK'] == 'Success') {
 	$mail->Send();
 
 	$log = __DIR__.$fPath.'purchases.log';
-	//$data = $email."\t".$name."\t$".$amount."\n";
-	$data = $date."\t".$amount."\t".'paypal'."\t".$name."\t".$email."\t".get_client_ip()."\n";
+	$data = date('M j, Y', $date)."\t".$amount."\t".'paypal'."\t".$name."\t".$email."\t".get_client_ip()."\n";
 	file_put_contents($log, $data, FILE_APPEND | LOCK_EX);
 	
 	
