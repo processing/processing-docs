d220ebcf5d067dd6cbd50c2cdd3ac7b8c3bacea7
amongiants <jon@folderstudio.com>
2014-06-06 16:25:11 -0700

    adding additional customer data to purchase log file

--- download/_ppEmailThanks.php
+++ download/_ppEmailThanks.php
@@ -5,6 +5,9 @@ require_once('./paypal/httprequest.php');
 //Load PHPMailer Class
 require_once('phpmailer/class.phpmailer.php');
 
+//Load Helpers for the ip address function
+require_once('./_helpers.php');
+
 //Use this form for production server 
 $r = new PayPal(true);
 
@@ -54,7 +57,8 @@ if ($final['ACK'] == 'Success') {
 	$mail->Send();
 
 	$log = __DIR__.$fPath.'purchases.log';
-	$data = $email."\t".$name."\t$".$amount."\n";
+	//$data = $email."\t".$name."\t$".$amount."\n";
+	$data = $date."\t".$amount."\t".'paypal'."\t".$name."\t".$email."\t".get_client_ip()."\n";
 	file_put_contents($log, $data, FILE_APPEND | LOCK_EX);
 	
 	
