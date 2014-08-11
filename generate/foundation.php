9790b607929bd11746d6b55589fec8492dea2ea0
philiptr <martin@Martins-iMac.local>
2014-08-11 15:11:08 -0700

    Change filenames to prevent overwriting

--- generate/foundation.php
+++ generate/foundation.php
@@ -24,58 +24,42 @@ $source = CONTENTDIR."foundation-test/";
 
 $page = new Page("Overview", "Overview");
 $page->content(file_get_contents($source."overview.html"));
-writeFile('overview.html', $page->out());
+writeFile('overviewf/index.html', $page->out());
 #copydirr($source.'/images', $path.'/images');
 
 $page = new Page("Mission", "Mission");
 $page->content(file_get_contents($source."mission.html"));
-writeFile('mission.html', $page->out());
+writeFile('mission/index.html', $page->out());
 
 $page = new Page("Projects", "Projects");
 $page->content(file_get_contents($source."projects.html"));
-writeFile('projects.html', $page->out());
+writeFile('projects/index.html', $page->out());
 
 $page = new Page("People", "People");
 $page->content(file_get_contents($source."people.html"));
-writeFile('people.html', $page->out());
+writeFile('peoplef/index.html', $page->out());
 
 $page = new Page("Fellowships", "Fellowships");
 $page->content(file_get_contents($source."fellowships.html"));
-writeFile('fellowships.html', $page->out());
+writeFile('fellowships/index.html', $page->out());
 
 $page = new Page("Reports", "Reports");
 $page->content(file_get_contents($source."reports.html"));
-writeFile('reports.html', $page->out());
+writeFile('reports/index.html', $page->out());
 
 $page = new Page("Patrons", "Patrons");
 $page->content(file_get_contents($source."patrons.html"));
-writeFile('patrons.html', $page->out());
+writeFile('patrons/index.html', $page->out());
 
 $page = new Page("Donate", "Donate");
 $page->content(file_get_contents($source."donate.html"));
-writeFile('donate.html', $page->out());
+writeFile('donate/index.html', $page->out());
 
 // Copy over the errata file for Processing: A Programming Handbook...
 copy($source.'processing-errata.txt', $path.'books/processing-errata.txt');
 // Copy over the media.zip file for Getting Started with Processing...
 copy($source.'media.zip', $path.'books/media.zip');
 
-// Copy over the images for the shop index
-if (!is_dir($path.'shop')) { 
-	mkdir($path.'shop', 0757);
-}
-
-if (!is_dir($path.'shop/imgs')) { 
-	mkdir($path.'shop/imgs', 0757); 
-}
-
-if (is_dir($path.'shop/imgs')) { 
-	copydirr($source.'shop/imgs', $path.'shop/imgs', null, 0757, false);
-}
-
-$page = new Page("Shop", "Shop");
-$page->content(file_get_contents($source.'shop/'."index.html"));
-writeFile('shop/index.html', $page->out());
 
 $benchmark_end = microtime_float();
 $execution_time = round($benchmark_end - $benchmark_start, 4);
