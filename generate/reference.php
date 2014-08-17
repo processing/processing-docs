ac4774c75d74103f4917593cf0f58517231dd4a5
Scott Murray <github@alignedleft.com>
2014-08-17 10:48:19 -0700

    Modified reference build script to pull latest changes from processing/processing

--- generate/reference.php
+++ generate/reference.php
@@ -6,7 +6,12 @@ $benchmark_start = microtime_float();
 
 $path = BASEDIR;  //define('BASEDIR',       dirname(__FILE__).'/');
 
-// Pull from GitHub
+// Pull latest processing/processing from GitHub
+// Note that the Reference generate script needs this,
+// just in case someone changed anything in the .java source files.
+`cd $path && /usr/bin/git pull https://github.com/processing/processing/`;
+
+// Pull latest processing/processing-docs from GitHub
 `cd $path && /usr/bin/git pull https://github.com/processing/processing-docs/`;
 
 $referencepath = $path . "java_generate/ReferenceGenerator/";
