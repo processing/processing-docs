0027df8ca8a77da2a822cf15d8a0eaef1463ee67
Scott Murray <github@alignedleft.com>
2014-08-17 14:43:43 -0700

    Finishing touches on reference generation script, fixes #113

--- generate/reference.php
+++ generate/reference.php
@@ -9,16 +9,18 @@ $path = BASEDIR;  //define('BASEDIR',       dirname(__FILE__).'/');
 // Pull latest processing/processing from GitHub
 // Note that the Reference generate script needs this,
 // just in case someone changed anything in the .java source files.
+echo '<p>Pulling in latest changes from processing/processing/...</p>';
 $mainRepoPath = $path . '../processing';
-echo '<p>' . $mainRepoPath . '</p>';
 `cd $mainRepoPath && /usr/bin/git pull https://github.com/processing/processing/`;
 
 // Pull latest processing/processing-docs from GitHub
+echo '<p>Pulling in latest changes from processing/processing-docs/...</p>';
 `cd $path && /usr/bin/git pull https://github.com/processing/processing-docs/`;
 
 $referencepath = $path . "java_generate/ReferenceGenerator/";
 
 // 
+echo '<p>Rebuilding reference files...</p>';
 `cd $referencepath && ./processingrefBuild.sh`;
 
 $benchmark_end = microtime_float();
