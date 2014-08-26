5e309c14e0cc985354df5b92547788efc3e0ad04
Ben Fry <fry@processing.org>
2014-08-26 19:31:22 -0400

    add 3.0a3

--- download/_downloads.php
+++ download/_downloads.php
@@ -152,6 +152,16 @@
 		  <li>No pre-release versions are currently available.</li>
 -->
 			<li>
+				<span class="version">3.0a3</span>
+				<span class="version-date">(26 August 2014)</span>
+				<a href="http://download.processing.org/processing-3.0a3-windows32.zip">Win 32</a>
+				<a href="http://download.processing.org/processing-3.0a3-windows64.zip">Win 64</a> 
+				<a href="http://download.processing.org/processing-3.0a3-linux32.tgz">Linux 32</a> 
+				<a href="http://download.processing.org/processing-3.0a3-linux64.tgz">Linux 64</a>
+				<a href="http://download.processing.org/processing-3.0a3-macosx.zip">Mac OS X</a> 
+			</li>
+
+			<li>
 				<span class="version">3.0a2</span>
 				<span class="version-date">(31 July 2014)</span>
 				<a href="http://download.processing.org/processing-3.0a2-windows32.zip">Win 32</a>
@@ -161,6 +171,7 @@
 				<a href="http://download.processing.org/processing-3.0a2-macosx.zip">Mac OS X</a> 
 			</li>
 
+<!--
 			<li>
 				<span class="version">3.0a1</span>
 				<span class="version-date">(26 July 2014)</span>
@@ -170,6 +181,7 @@
 				<a href="http://download.processing.org/processing-3.0a1-linux64.tgz">Linux 64</a>
 				<a href="http://download.processing.org/processing-3.0a1-macosx.zip">Mac OS X</a> 
 			</li>
+-->
 
 			<!-- Temp fix, now commented out
 			<li>
