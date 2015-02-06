a15b3969f408a1e3ed3db671b19580115f733c04
Daniel Shiffman <daniel@shiffman.net>
2015-02-06 16:01:13 +0000

    Control examples to p5 #154

--- content/examples_p5/Basics/Control/Conditionals2/Conditionals2.js
+++ content/examples_p5/Basics/Control/Conditionals2/Conditionals2.js
@@ -7,22 +7,26 @@
  * action. 
  */
  
-size(640, 360);
-background(0);
+function setup() {
 
-for(int i = 2; i < width-2; i += 2) {
-  // If 'i' divides by 20 with no remainder
-  if((i % 20) == 0) {
-    stroke(255);
-    line(i, 80, i, height/2);
-  // If 'i' divides by 10 with no remainder
-  } else if ((i % 10) == 0) {
-    stroke(153);
-    line(i, 20, i, 180); 
-  // If neither of the above two conditions are met
-  // then draw this line
-  } else {  
-    stroke(102);
-    line(i, height/2, i, height-20);
+  createCanvas(640, 360);
+  background(0);
+
+  for(var i = 2; i < width-2; i += 2) {
+    // If 'i' divides by 20 with no remainder
+    if((i % 20) == 0) {
+      stroke(255);
+      line(i, 80, i, height/2);
+    // If 'i' divides by 10 with no remainder
+    } else if ((i % 10) == 0) {
+      stroke(153);
+      line(i, 20, i, 180); 
+    // If neither of the above two conditions are met
+    // then draw this line
+    } else {  
+      stroke(102);
+      line(i, height/2, i, height-20);
+    }
   }
+  noLoop();
 }
--- content/examples_p5/Basics/Control/EmbeddedIteration/EmbeddedIteration.js
+++ content/examples_p5/Basics/Control/EmbeddedIteration/EmbeddedIteration.js
@@ -5,20 +5,22 @@
  *
  */
 
+function setup() {
+  createCanvas(640, 360); 
+  background(0); 
+  noStroke(); 
 
-size(640, 360); 
-background(0); 
-noStroke(); 
+  var gridSize = 40;
 
-int gridSize = 40;
-
-for (int x = gridSize; x <= width - gridSize; x += gridSize) {
-  for (int y = gridSize; y <= height - gridSize; y += gridSize) {
-    noStroke();
-    fill(255);
-    rect(x-1, y-1, 3, 3);
-    stroke(255, 50);
-    line(x, y, width/2, height/2);
+  for (var x = gridSize; x <= width - gridSize; x += gridSize) {
+    for (var y = gridSize; y <= height - gridSize; y += gridSize) {
+      noStroke();
+      fill(255);
+      rect(x-1, y-1, 3, 3);
+      stroke(255, 50);
+      line(x, y, width/2, height/2);
+    }
   }
-}
 
+  noLoop();
+}
