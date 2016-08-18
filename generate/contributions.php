2970f4443409709e436913fd306a8fa01b2c5ce1
Elie Zananiri <prisonerjohn@gmail.com>
2016-08-18 09:41:49 +0200

    Revert "Update contributions generation script."
    This reverts commit c5b058920b383820cd7cc9dc9d5d578a10b99c84.

--- generate/contributions.php
+++ generate/contributions.php
@@ -10,7 +10,7 @@ require_once('../config.php');
 //$benchmark_start = microtime_float();
 
 //Grab the contributions data from the text file
-$contributions  = "\n".file_get_contents('../contrib_generate/contribs.txt');
+$contributions  = "\n".file_get_contents('../contrib_generate/contributions.txt');
 //$contributions .= file_get_contents('../contrib_generate/legacy.txt');
 
 //Split the contributions text file by contribution type
@@ -34,7 +34,7 @@ foreach($byType as $type => &$contents){
 	$count = 0;
 	foreach($contents as &$entity){
 		$entity = preg_split( "/(\n)/", $entity);
-		array_shift($entity);
+		array_shift($entity); 
 		array_pop($entity);
 
 		foreach($entity as &$line){
@@ -77,14 +77,14 @@ function anchorSafe($input){
 //Push all libraries into array sorted by category
 $categories = array();
 foreach($clean['library'] as $library){
-	array_push($categories, $library['categories']);
+	array_push($categories, $library['category']);
 }
 $categories = array_unique($categories);
 sort($categories);
 
 //Create a nav from each category (Except Legacy)
 $librariesHTML = '<div class="contributions">'."\n".'<div class="categories">'."\n";
-foreach($categories as $category){
+foreach($categories as $category){ 
 	if($category != 'Legacy'){
 		$librariesHTML .= "\t".'<a href="#'.anchorSafe($category).'">'.$category.'</a><br>'."\n";
 	}
@@ -97,12 +97,12 @@ foreach($categories as $category){
 		$librariesHTML .= '<div id="'.anchorSafe($category).'">'."\n\t";
 		$librariesHTML .= '<h4>'.$category.'</h4>'."\n";
 		$librariesHTML .= "\t<ul>\n";
-		$libs = array_filter($clean['library'], function($obj) use($category){ return $obj['categories'] == $category; });
-
+		$libs = array_filter($clean['library'], function($obj) use($category){ return $obj['category'] == $category; });
+		
 		foreach($libs as $lib) {
 			$librariesHTML .= "\t\t<li>\n";
 			$librariesHTML .= "\t\t\t".'<h5><a href="'.$lib['url'].'">'.$lib['name'].'</a></h5>'."\n";
-			$librariesHTML .= "\t\t\t".'<span>by '.linkParse($lib['authors']).'</span>'."\n";
+			$librariesHTML .= "\t\t\t".'<span>by '.linkParse($lib['authorList']).'</span>'."\n";
 			$librariesHTML .= strlen(trim($lib['sentence'])) ? "\t\t\t".'<p>'.linkParse($lib['sentence']).'</p>'."\n" : "\n";
 			$librariesHTML .= "\t\t</li>\n";
 		}
@@ -130,9 +130,9 @@ $librariesHTML .= "</div>";
 //Write LibrariesHTML to libraries.html file
 $file_dir 	= CONTENTDIR.'static/';
 $file_name 	= strip_tags('libraries.html');
-$file 		= $file_dir.$file_name;
+$file 		= $file_dir.$file_name; 
 
-$create_file = fopen($file, "w+");
+$create_file = fopen($file, "w+"); 
 $chmod = chmod($file, 0755);
 fwrite($create_file, $librariesHTML);
 fclose($create_file);
@@ -146,10 +146,10 @@ $toolsHTML 	= "<div class='contributions'>\n<ul>\n";
 //$legacy 	= "<div class='legacy'>\n<h4>Legacy Tools</h4>\n<p>The tools in this category haven't been updated by their creators to be compatible with the Processing 2 library manager and/or updated to work with Processing 2. We hope to have all tools working with Processing 2; if one of your favorite tools hasn't been updated, we encourage you to contact the tool creator or to update it yourself and share it. Instructions for creating a Processing 2 library <a href='https://github.com/processing/processing/wiki'>are on the Processing GitHub site.</a>.</p>\n<ul>\n";
 
 foreach($clean['tool'] as $tool){
-	//if($tool['categories'] != 'Legacy'){
+	//if($tool['category'] != 'Legacy'){
 		$toolsHTML .= "\t<li>\n";
 		$toolsHTML .= "\t\t".'<h5><a href="'.$tool['url'].'">'.$tool['name'].'</a></h5>'."\n";
-		$toolsHTML .= "\t\t".'<span>by '.linkParse($tool['authors']).'</span>'."\n";
+		$toolsHTML .= "\t\t".'<span>by '.linkParse($tool['authorList']).'</span>'."\n";
 		$toolsHTML .= strlen(trim($tool['sentence'])) ? "\t\t".'<p>'.linkParse($tool['sentence']).'</p>'."\n" : "\n";
 		$toolsHTML .= "\t</li>\n";
 	//} else {
@@ -166,9 +166,9 @@ $toolsHTML .= "</div>";
 
 //Write toolsHTML to tools.html file
 $file_name 	= strip_tags('tools.html');
-$file 		= $file_dir.$file_name;
+$file 		= $file_dir.$file_name; 
 
-$create_file = fopen($file, "w+");
+$create_file = fopen($file, "w+"); 
 $chmod = chmod($file, 0755);
 fwrite($create_file, $toolsHTML);
 fclose($create_file);
