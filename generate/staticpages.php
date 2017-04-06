0c2966045c325d92968976dcb33ebc288ddb404c
REAS <reas@reas.com>
2017-04-06 10:10:50 -0700

    New download page

--- generate/staticpages.php
+++ generate/staticpages.php
@@ -51,8 +51,8 @@ writeFile('download/support.html', $page->out());
 
 // Added 6 April 2017 for Membership Initiative
 $page = new Page("Download", "Download");
-$page->content(file_get_contents($source."download-test.html"));
-writeFile('download/download-test.html', $page->out());
+$page->content(file_get_contents($source."download.html"));
+writeFile('download/index.html', $page->out());
 
 
 // Copy over the errata file for Processing: A Programming Handbook...
