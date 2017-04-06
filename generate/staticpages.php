62bba0d6fb0319db60315f2175143526a182db90
REAS <reas@reas.com>
2017-04-06 08:18:39 -0700

    Add test for new download page

--- generate/staticpages.php
+++ generate/staticpages.php
@@ -45,9 +45,15 @@ $page->content(file_get_contents($source."handbook.html"));
 writeFile('handbook/index.html', $page->out());
 
 // Added 4 April 2017 for Membership Initiative
-$page = new Page("Membership", "Download");
+$page = new Page("Support", "Download");
 $page->content(file_get_contents($source."membership.html"));
 writeFile('download/support.html', $page->out());
+
+// Added 6 April 2017 for Membership Initiative
+$page = new Page("Download", "Download");
+$page->content(file_get_contents($source."download-test.html"));
+writeFile('download/download-test.html', $page->out());
+
 
 // Copy over the errata file for Processing: A Programming Handbook...
 // copy($source.'processing-errata.txt', $path.'books/processing-errata.txt');
